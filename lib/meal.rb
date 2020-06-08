e590a1f8d645daadab313c3da43b900344c03a20
iona-b <ionabrabender@gmail.com>
2020-06-07 18:00:16 -0700

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
 
+    attr_reader :waiter, :customer, :total, :tip
+
+    @@all = []
+
+    def initialize(waiter, customer, total, tip) # initializes with a waiter, a customer, a total and a tip
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all # is class method that returns the contents of @@all
+        @@all
+    end
+
 end
